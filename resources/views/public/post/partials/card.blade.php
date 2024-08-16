e8e352809e5655f60d419507ec5afffb655fed9f
fxcjahid <fxcjahid3@gmail.com>
2024-08-16 23:46:23 +0600

    Changes to be committed: 	modified:   public/assets/admin/css/admin.css 	modified:   public/assets/public/css/app.css 	modified:   public/mix-manifest.json 	modified:   resources/views/public/post/partials/card.blade.php

--- resources/views/public/post/partials/card.blade.php
+++ resources/views/public/post/partials/card.blade.php
@@ -1,10 +1,10 @@
 <article role="article" class="md:flex gap-x-4 border-b-2 px-2 py-3 md:p-4 hover:border-slate-100 hover:shadow-md">
     @if ($post->image())
-        <img src="{{ $post->image() }}" class="relative z-0 lg:h-60 lg:max-w-xs rounded-md object-cover"
+        <img src="{{ $post->image() }}" class="relative z-0 lg:h-60 lg:max-w-[15rem] rounded-md object-cover"
             alt="{{ $post->title }}">
     @endif
 
-    <div class="md:mt-3 px-2">
+    <div class="mt-4 md:mt-3 px-2">
         <a href="{{ $post->url() }}">
             <h1 role="title" title="{{ $post->title }}"
                 class="ellipsis text-lg font-semibold text-slate-900 hover:text-blue-900 dark:text-white md:text-2xl">
